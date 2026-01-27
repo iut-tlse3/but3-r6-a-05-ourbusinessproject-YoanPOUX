572de5b8dc3765c686e91950fa9db749db58316d
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2026-01-27 14:50:36 +0100

    fix #1.2 classe Enterprise

---
+++ src/main/java/ourbuisnessproject/Enterprise.java
@@ -0,0 +1,68 @@
+package ourbuisnessproject;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank
+    private String name;
+    @Size(min = 10)
+    private String description;
+    @NotBlank
+    private String contactName;
+    @NotBlank @Email
+    private String contactEmail;
+
+    /**
+     *
+     * @param name ..
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
