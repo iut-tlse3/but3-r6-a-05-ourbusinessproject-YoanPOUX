59e82a475f7fa7267367f54b96fcb1219024a2c4
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2026-01-27 10:49:17 +0100

    fix #1.1 classe Project

--- src/main/java/ourbuisnessproject/Project.java
+++ src/main/java/ourbuisnessproject/Project.java
@@ -12,11 +12,35 @@ public class Project {
         this.description = "default description";
     }
 
+    /**
+     * Set the title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Return the title
+     * @return title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Set the description
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    /**
+     * Return the description
+     * @return desciption
+     */
+    public String getDescription() {
+        return description;
     }
 }
