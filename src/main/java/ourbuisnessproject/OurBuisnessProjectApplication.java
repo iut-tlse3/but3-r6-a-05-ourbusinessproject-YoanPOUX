eb150832686938304dd021c4a326ec821fbb0a45
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2026-01-27 08:50:31 +0100

    initial commit spring boot project

---
+++ src/main/java/ourbuisnessproject/OurBuisnessProjectApplication.java
@@ -0,0 +1,13 @@
+package ourbuisnessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBuisnessProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurBuisnessProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ourbuisnessproject/OurBuisnessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ourbuisnessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBuisnessProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
