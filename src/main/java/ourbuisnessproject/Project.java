2efa7def1613299f4d331b7d3a0d567b607ce472
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2026-01-27 14:55:11 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbuisnessproject/Project.java
+++ src/main/java/ourbuisnessproject/Project.java
@@ -1,8 +1,17 @@
 package ourbuisnessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     @NotBlank
     private String title;
     private String description;
@@ -10,6 +19,14 @@ public class Project {
     public Project() {
         this.title = "defaut title";
         this.description = "default description";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     /**
